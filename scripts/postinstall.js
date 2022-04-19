655d29ca5d92185047b306b6598b411eeca7ed05
tcpglobalc <tcpglobal@protonmail.com>
2022-04-19 12:39:21 +0800

    123

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,10 +2,12 @@ const fs = require('fs')
 const path = require('path')
 const prettier = require('prettier')
 
-const editorPath = path.resolve(__dirname, process.argv[2] || require.resolve('decentraland-ecs/artifacts/editor.js'))
+// const editorPath = path.resolve(__dirname, process.argv[2] || require.resolve('decentraland-ecs/artifacts/editor.js'))
+const editorPath = '/root/decentraland/builder/node_modules/decentraland-ecs/editor.js';
 const targetEditorPath = path.resolve(__dirname, '../public/editor.js')
 
-const ecsPath = path.resolve(__dirname, process.argv[3] || require.resolve('decentraland-ecs/dist/src/index.js'))
+// const ecsPath = path.resolve(__dirname, process.argv[3] || require.resolve('decentraland-ecs/dist/src/index.js'))
+const ecsPath = '/root/decentraland/builder/node_modules/decentraland-ecs/dist/src/index.js';
 const targetEcsPath = path.resolve(__dirname, '../src/ecsScene/ecs.js.raw')
 
 console.log(`> Copying file ${editorPath} into ${targetEditorPath}`)
